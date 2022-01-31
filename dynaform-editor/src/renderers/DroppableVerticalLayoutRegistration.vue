54a6ef48b7e6fba669c8c2a5e364199a52522873
Dheeyi William <dheeyi@gmail.com>
2022-01-31 11:40:16 -0400

    drag enabled for vertical layout

--- dynaform-editor/src/renderers/DroppableVerticalLayoutRegistration.vue
+++ dynaform-editor/src/renderers/DroppableVerticalLayoutRegistration.vue
@@ -1,33 +1,36 @@
 <template>
-
-   <v-container
-    v-if="layout.visible"
-    fill-height
-  >
-  <h3>vertical layout</h3>
-    <v-row
-      v-for="(element, index) in layout.uischema.elements"
-      :key="`${layout.path}-${index}`"
-      no-gutters
+  <div>
+    <draggable
+      :class="draggableClass"
+      :list="[]"
+      group="people"
+      @change="handleChange"
     >
-      <v-col cols="12" :class="styles.verticalLayout.item">
-        <dispatch-renderer v-for="child in uischema.elements" :key="child.uuid"
-          :schema="schema"
-          :uischema="child"
-          :path="path"
-          :enabled="enabled"
-          :renderers="[...renderers, DroppableElementRegistration]"
-          :cells="cells"
-        />
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-row
+        v-for="(element, index) in uischema.elements"
+        :key="`${layout.path}-${index}`"
+        no-gutters
+      >
+        <v-col cols="12" :class="styles.verticalLayout.item">
+          <dispatch-renderer
+            :key="element.uuid"
+            :schema="layout.schema"
+            :uischema="element"
+            :path="layout.path"
+            :enabled="layout.enabled"
+            :renderers="customRenderers"
+            :cells="layout.cells"
+          />
+        </v-col>
+      </v-row>
+    </draggable>
+  </div>
 </template>
 
 <script lang="ts">
+import draggable from 'vuedraggable';
 import {
   uiTypeIs,
-  JsonFormsRendererRegistryEntry,
   Layout,
   rankWith,
 } from '@jsonforms/core';
@@ -40,7 +43,10 @@ import {
 } from '@jsonforms/vue2';
 import { useVuetifyLayout } from '@jsonforms/vue2-vuetify';
 import { VContainer, VRow, VCol } from 'vuetify/lib';
-import  {DroppableElementRegistration}  from './DroppableElement.vue';
+import { DroppableElementRegistration } from "@/renderers/DroppableElement.vue";
+import { EditorUISchemaElement } from "@/model";
+import { createControl } from "@/util";
+
 const layoutRenderer = defineComponent({
   name: 'vertical-layout-renderer',
   components: {
@@ -48,7 +54,7 @@ const layoutRenderer = defineComponent({
     VContainer,
     VRow,
     VCol,
-    DroppableElementRegistration
+    draggable,
   },
   props: {
     ...rendererProps<Layout>(),
@@ -56,6 +62,40 @@ const layoutRenderer = defineComponent({
   setup(props: RendererProps<Layout>) {
     return useVuetifyLayout(useJsonFormsLayout(props));
   },
+  computed: {
+    draggableClass(): string {
+      return 'dragArea list-group';
+    },
+    customRenderers(): Array<any> {
+      return (
+          this.renderers && [...this.renderers, DroppableElementRegistration]
+      );
+    },
+  },
+  methods: {
+    handleChange(evt: any) {
+      if (evt.added) {
+        if (evt.added.element.uuid) {
+          const uiSchemaElement: EditorUISchemaElement = createControl(
+              evt.added.element
+          );
+          this.$store.dispatch('app/addScopedElementToLayout', {
+            uiSchemaElement: uiSchemaElement,
+            layoutUUID: this.uischema.uuid,
+            index: 0,
+            schemaUUID: evt.added.element.uuid,
+          });
+        } else {
+          let provider = evt.added.element.uiSchemaElementProvider();
+          this.$store.dispatch('app/addUnscopedElementToLayout', {
+            uiSchemaElement: provider,
+            layoutUUID: this.uischema.uuid,
+            index: 0,
+          });
+        }
+      }
+    },
+  }
 });
 
 export default layoutRenderer;
