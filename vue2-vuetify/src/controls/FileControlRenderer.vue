7b1feb08fbe8f6e8692a64ecb4ae65152c42d83f
kchobantonov <kchobantonov@gmail.com>
2022-03-02 17:52:30 -0500

    fix disabled-icon-focus

--- vue2-vuetify/src/controls/FileControlRenderer.vue
+++ vue2-vuetify/src/controls/FileControlRenderer.vue
@@ -7,7 +7,7 @@
   >
     <v-hover v-slot="{ hover }">
       <v-file-input
-        v-disabled-icon-focus
+        disabled-icon-focus
         :id="control.id + '-input'"
         :class="styles.control.input"
         :disabled="!control.enabled"
