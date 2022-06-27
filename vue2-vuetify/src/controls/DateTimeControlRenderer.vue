11dba4a08ab1946fb9d2891bd7a240d94ac45143
kchobantonov <kchobantonov@gmail.com>
2022-06-27 09:09:12 -0400

    add updated renderers for date/date-time/time controls

--- vue2-vuetify/src/controls/DateTimeControlRenderer.vue
+++ vue2-vuetify/src/controls/DateTimeControlRenderer.vue
@@ -5,83 +5,377 @@
     :isFocused="isFocused"
     :appliedOptions="appliedOptions"
   >
-    <v-text-field
-      type="datetime-local"
-      :id="control.id + '-input'"
-      :class="styles.control.input"
-      :disabled="!control.enabled"
-      :autofocus="appliedOptions.focus"
-      :placeholder="appliedOptions.placeholder"
-      :label="computedLabel"
-      :hint="control.description"
-      :persistent-hint="persistentHint()"
-      :required="control.required"
-      :error-messages="control.errors"
-      :value="dataTime"
-      v-bind="vuetifyProps('v-text-field')"
-      @focus="isFocused = true"
-      @blur="isFocused = false"
-    />
+    <v-menu
+      ref="menu"
+      v-model="showMenu"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      offset-y
+    >
+      <template v-slot:activator="{ on: onMenu }">
+        <v-hover v-slot="{ hover }">
+          <v-text-field
+            disabled-icon-focus
+            :id="control.id + '-input'"
+            :class="styles.control.input"
+            :disabled="!control.enabled"
+            :autofocus="appliedOptions.focus"
+            :placeholder="appliedOptions.placeholder"
+            :label="computedLabel"
+            :hint="control.description"
+            :persistent-hint="persistentHint()"
+            :required="control.required"
+            :error-messages="control.errors"
+            v-bind="vuetifyProps('v-text-field')"
+            v-mask="mask"
+            v-on="onMenu"
+            :value="inputValue"
+            @change="onInputChange"
+            @focus="isFocused = true"
+            @blur="isFocused = false"
+          >
+            <template slot="append">
+              <v-icon v-if="hover" @click="clear">mdi-close</v-icon>
+            </template>
+          </v-text-field>
+        </v-hover>
+      </template>
+      <v-card v-if="showMenu">
+        <v-row no-gutters>
+          <v-col min-width="290px">
+            <v-date-picker
+              v-if="showMenu"
+              :value="datePickerValue"
+              ref="datePicker"
+              v-bind="vuetifyProps('v-date-picker')"
+              :min="min"
+              :max="max"
+            >
+            </v-date-picker>
+          </v-col>
+          <v-col min-width="290px">
+            <v-time-picker
+              :value="timePickerValue"
+              ref="timePicker"
+              v-bind="vuetifyProps('v-time-picker')"
+              :min="min"
+              :max="max"
+              :use-seconds="useSeconds"
+              :format="ampm ? 'ampm' : '24hr'"
+            ></v-time-picker>
+          </v-col>
+        </v-row>
+        <v-card-actions>
+          <v-btn text color="primary" @click="clear"> {{ clearLabel }} </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn text @click="showMenu = false">
+            {{ cancelLabel }}
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="
+              () => {
+                onPickerChange(
+                  $refs.datePicker.inputDate,
+                  $refs.timePicker.genValue()
+                );
+                showMenu = false;
+              }
+            "
+          >
+            {{ okLabel }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
   </control-wrapper>
 </template>
 
 <script lang="ts">
 import {
   ControlElement,
-  JsonFormsRendererRegistryEntry,
-  rankWith,
   isDateTimeControl,
+  JsonFormsRendererRegistryEntry,
+  JsonSchema,
+  rankWith,
 } from '@jsonforms/core';
-import { defineComponent } from '../vue';
 import {
   rendererProps,
-  useJsonFormsControl,
   RendererProps,
+  useJsonFormsControl,
 } from '@jsonforms/vue2';
+import {
+  VBtn,
+  VDatePicker,
+  VHover,
+  VIcon,
+  VMenu,
+  VSpacer,
+  VTextField,
+  VTimePicker,
+  VRow,
+  VCol,
+  VCard,
+  VCardTitle,
+  VCardActions,
+} from 'vuetify/lib';
+import { parseDateTime, useTranslator, useVuetifyControl } from '../util';
+import { defineComponent } from '../vue';
 import { default as ControlWrapper } from './ControlWrapper.vue';
-import { useVuetifyControl, parseDateTime } from '../util';
-import { VTextField } from 'vuetify/lib';
+import { DisabledIconFocus } from './directives';
+import { VueMaskDirective as Mask } from 'v-mask';
+import { ref } from '@vue/composition-api';
+import dayjs from 'dayjs';
+
+const JSON_SCHEMA_DATE_TIME_FORMATS = [
+  "YYYY-MM-DD'T'HH:mm:ss.SSSZ",
+  "YYYY-MM-DD'T'HH:mm:ss.SSS",
+  "YYYY-MM-DD'T'HH:mm:ssZ",
+  "YYYY-MM-DD'T'HH:mm:ss",
+];
+
+// https://ajv.js.org/packages/ajv-formats.html#keywords-to-compare-values-formatmaximum-formatminimum-and-formatexclusivemaximum-formatexclusiveminimum
+type AjvMinMaxFormat = {
+  formatMinimum?: string | { $data: any };
+  formatExclusiveMinimum?: string | { $data: any };
+  formatMaximum?: string | { $data: any };
+  formatExclusiveMaximum?: string | { $data: any };
+};
 
 const controlRenderer = defineComponent({
   name: 'datetime-control-renderer',
   components: {
     ControlWrapper,
+    VBtn,
+    VDatePicker,
+    VTimePicker,
+    VHover,
+    VIcon,
+    VMenu,
+    VSpacer,
     VTextField,
+    VRow,
+    VCol,
+    VCard,
+    VCardTitle,
+    VCardActions,
   },
+  directives: { DisabledIconFocus, Mask },
   props: {
     ...rendererProps<ControlElement>(),
   },
   setup(props: RendererProps<ControlElement>) {
-    return useVuetifyControl(
-      useJsonFormsControl(props),
-      (value) => value || undefined,
-      300
+    const t = useTranslator();
+    const showMenu = ref(false);
+    const mask = ref<((value: string) => (string | RegExp)[]) | undefined>(
+      undefined
     );
+    const control = useVuetifyControl(
+      useJsonFormsControl(props),
+      (value) => value || undefined
+    );
+    return { ...control, showMenu, mask, t };
+  },
+  watch: {
+    isFocused(newFocus) {
+      if (newFocus) {
+        //this.mask = this.maskFunction;
+      } else {
+        this.mask = undefined;
+      }
+    },
   },
   computed: {
-    dataTime: {
-      get(): string | null | undefined {
-        const datetimeLocalFormat = 'YYYY-MM-DDTHH:mm:ss.SSS';
-        const saveFormat = this.appliedOptions.dateTimeSaveFormat ?? undefined;
-        const value = this.control.data as string | undefined | null;
+    dateTimeFormat(): string {
+      return typeof this.appliedOptions.dateTimeFormat == 'string'
+        ? this.appliedOptions.dateTimeFormat
+        : 'YYYY-MM-DD HH:mm';
+    },
+    dateTimeSaveFormat(): string {
+      return typeof this.appliedOptions.dateTimeSaveFormat == 'string'
+        ? this.appliedOptions.dateTimeSaveFormat
+        : 'YYYY-MM-DD HH:mm';
+    },
+    formats(): string[] {
+      return [
+        this.dateTimeSaveFormat,
+        this.dateTimeFormat,
+        ...JSON_SCHEMA_DATE_TIME_FORMATS,
+      ];
+    },
+    useSeconds(): boolean {
+      return this.dateTimeFormat.includes('s') ? true : false;
+    },
+    ampm(): boolean {
+      return this.appliedOptions.ampm === true;
+    },
+    min(): string | undefined {
+      // provide min so that the browser can display the native component with only selections that are allowed.
+      // Since the browser supports only min there is posibility for the user to select a date that is defined in the formatExclusiveMinimum but the ajv will catch that validation.
+      const schema = this.control.schema as JsonSchema & AjvMinMaxFormat;
+      if (typeof schema.formatMinimum === 'string') {
+        return schema.formatMinimum;
+      } else if (typeof schema.formatExclusiveMinimum === 'string') {
+        return schema.formatExclusiveMinimum;
+      }
+      return undefined;
+    },
+    max(): string | undefined {
+      // provide max so that the browser can display the native component with only selections that are allowed.
+      // Since the browser supports only max there is posibility for the user to select a date that is defined in the formatExclusiveMaximum but the ajv will catch that validation.
+      const schema = this.control.schema as JsonSchema & AjvMinMaxFormat;
+      if (typeof schema.formatMaximum === 'string') {
+        return schema.formatMaximum;
+      } else if (typeof schema.formatExclusiveMaximum === 'string') {
+        return schema.formatExclusiveMaximum;
+      }
+      return undefined;
+    },
+    inputValue(): string | undefined {
+      const value = this.control.data;
+      const date = parseDateTime(value, this.formats);
+      return date ? date.format(this.dateTimeFormat) : value;
+    },
+    datePickerValue(): string | undefined {
+      const value = this.control.data;
 
-        const dateTime = parseDateTime(value, saveFormat);
-        return dateTime ? dateTime.local().format(datetimeLocalFormat) : value;
-      },
-      set(value: string) {
-        const datetimeLocalFormats = [
-          'YYYY-MM-DDTHH:mm:ss.SSS',
-          'YYYY-MM-DDTHH:mm:ss',
-          'YYYY-MM-DDTHH:mm',
-        ];
-        const saveFormat =
-          this.appliedOptions.dateTimeSaveFormat ?? 'YYYY-MM-DDTHH:mm:ssZ';
+      const date = parseDateTime(value, this.formats);
+      return date ? date.format('YYYY-MM-DD') : value;
+    },
+    timePickerValue(): string | undefined {
+      const value = this.control.data;
 
-        const dateTime = parseDateTime(value, datetimeLocalFormats);
-        const result = dateTime ? dateTime.format(saveFormat) : value;
+      const time = parseDateTime(value, this.formats);
+      return time
+        ? this.useSeconds
+          ? time.format('HH:mm:ss')
+          : time.format('HH:mm')
+        : value;
+    },
+    clearLabel(): string {
+      const label =
+        typeof this.appliedOptions.clearLabel == 'string'
+          ? this.appliedOptions.clearLabel
+          : 'Clear';
 
-        this.onChange(result);
-      },
+      return label;
+    },
+    cancelLabel(): string {
+      const label =
+        typeof this.appliedOptions.cancelLabel == 'string'
+          ? this.appliedOptions.cancelLabel
+          : 'Cancel';
+
+      return label;
+    },
+    okLabel(): string {
+      const label =
+        typeof this.appliedOptions.okLabel == 'string'
+          ? this.appliedOptions.okLabel
+          : 'OK';
+      return label;
+    },
+  },
+  methods: {
+    onInputChange(value: string): void {
+      const date = parseDateTime(value, this.dateTimeFormat);
+      this.onChange(date ? date.format(this.dateTimeSaveFormat) : value);
+    },
+    onPickerChange(dateValue: string, timeValue: string): void {
+      const date = parseDateTime(dateValue, 'YYYY-MM-DD');
+      const time = parseDateTime(
+        timeValue,
+        this.useSeconds ? 'HH:mm:ss' : 'HH:mm'
+      );
+      if (date && time) {
+        const dateTimeString = `${date.format('YYYY-MM-DD')}T${time.format(
+          'HH:mm:ss.SSSZ'
+        )}`;
+        const dateTime = parseDateTime(
+          dateTimeString,
+          "YYYY-MM-DD'T'HH:mm:ss.SSSZ"
+        );
+        this.onChange(dateTime!.format(this.dateTimeSaveFormat));
+      }
+    },
+    clear(): void {
+      this.mask = undefined;
+      this.onChange(null);
+    },
+    maskFunction(value: string): (string | RegExp)[] {
+      const format = this.dateTimeFormat;
+
+      const parts = format.split(/([^YMD]*)(YYYY|YY|MMMM|MMM|MM|M|DD|D)/);
+
+      let index = -1;
+      const numbers = value?.replace(/[^0-9]/g, '');
+
+      let result: (string | RegExp)[] = [];
+      for (const part of parts) {
+        if (part && part !== '') {
+          if (part == 'YYYY') {
+            result.push(/[0-9]/);
+            result.push(/[0-9]/);
+            result.push(/[0-9]/);
+            result.push(/[0-9]/);
+            index += 4;
+          } else if (part == 'YY') {
+            result.push(/[0-9]/);
+            result.push(/[0-9]/);
+            index += 2;
+          } else if (part == 'M') {
+            result.push(/[1]/);
+            result.push(/[0-2]/);
+            index += 2;
+          } else if (part == 'MM') {
+            result.push(/[0-1]/);
+            index += 1;
+            result.push(numbers.charAt(index) === '0' ? /[1-9]/ : /[0-2]/);
+            index += 1;
+          } else if (part == 'MMM') {
+            let regex = '(';
+            for (let i = 0; i <= 11; i++) {
+              regex += dayjs().month(0).format('MMM');
+              if (i < 11) {
+                regex += '|';
+              }
+            }
+            regex += ')';
+            result.push(new RegExp(regex));
+          } else if (part == 'MMMM') {
+            let regex = '(';
+            for (let i = 0; i <= 11; i++) {
+              regex += dayjs().month(0).format('MMMM');
+              if (i < 11) {
+                regex += '|';
+              }
+            }
+            regex += ')';
+            result.push(new RegExp(regex));
+          } else if (part == 'D') {
+            result.push(/[1-3]/);
+            index += 1;
+            result.push(numbers.charAt(index) === '3' ? /[0-1]/ : /[0-9]/);
+            index += 1;
+          } else if (part == 'DD') {
+            result.push(/[0-3]/);
+            index += 1;
+            result.push(
+              numbers.charAt(index) === '3'
+                ? /[0-1]/
+                : numbers.charAt(index) === '0'
+                ? /[1-9]/
+                : /[0-9]/
+            );
+            index += 1;
+          } else {
+            result.push(part);
+          }
+        }
+      }
+
+      return result;
     },
   },
 });
@@ -93,3 +387,12 @@ export const entry: JsonFormsRendererRegistryEntry = {
   tester: rankWith(2, isDateTimeControl),
 };
 </script>
+
+<style scoped>
+.v-picker {
+  border-radius: 0px;
+}
+.v-picker > .v-picker__title {
+  min-height: 102px;
+}
+</style>
--- vue2-vuetify/src/controls/TimeControlRenderer.vue
+++ vue2-vuetify/src/controls/TimeControlRenderer.vue
@@ -5,59 +5,361 @@
     :isFocused="isFocused"
     :appliedOptions="appliedOptions"
   >
-    <v-text-field
-      type="time"
-      :id="control.id + '-input'"
-      :class="styles.control.input"
-      :disabled="!control.enabled"
-      :autofocus="appliedOptions.focus"
-      :placeholder="appliedOptions.placeholder"
-      :label="computedLabel"
-      :hint="control.description"
-      :persistent-hint="persistentHint()"
-      :required="control.required"
-      :error-messages="control.errors"
-      :value="control.data"
-      v-bind="vuetifyProps('v-text-field')"
-      @input="onChange"
-      @focus="isFocused = true"
-      @blur="isFocused = false"
-    />
+    <v-menu
+      ref="menu"
+      v-model="showMenu"
+      :close-on-content-click="false"
+      transition="scale-transition"
+      offset-y
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on: onMenu }">
+        <v-hover v-slot="{ hover }">
+          <v-text-field
+            disabled-icon-focus
+            :id="control.id + '-input'"
+            :class="styles.control.input"
+            :disabled="!control.enabled"
+            :autofocus="appliedOptions.focus"
+            :placeholder="appliedOptions.placeholder"
+            :label="computedLabel"
+            :hint="control.description"
+            :persistent-hint="persistentHint()"
+            :required="control.required"
+            :error-messages="control.errors"
+            v-bind="vuetifyProps('v-text-field')"
+            v-mask="mask"
+            v-on="onMenu"
+            :value="inputValue"
+            @change="onInputChange"
+            @focus="isFocused = true"
+            @blur="isFocused = false"
+          >
+            <template slot="append">
+              <v-icon v-if="hover" @click="clear">mdi-close</v-icon>
+            </template>
+          </v-text-field>
+        </v-hover>
+      </template>
+      <v-time-picker
+        v-if="showMenu"
+        :value="pickerValue"
+        ref="picker"
+        v-bind="vuetifyProps('v-time-picker')"
+        :min="min"
+        :max="max"
+        :use-seconds="useSeconds"
+        :format="ampm ? 'ampm' : '24hr'"
+      >
+        <v-btn text color="primary" @click="clear"> {{ clearLabel }} </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text color="primary" @click="showMenu = false">
+          {{ cancelLabel }}
+        </v-btn>
+        <v-btn
+          text
+          color="primary"
+          @click="
+            () => {
+              onPickerChange($refs.picker.genValue());
+              showMenu = false;
+            }
+          "
+        >
+          {{ okLabel }}
+        </v-btn></v-time-picker
+      >
+    </v-menu>
   </control-wrapper>
 </template>
 
 <script lang="ts">
 import {
   ControlElement,
-  JsonFormsRendererRegistryEntry,
-  rankWith,
   isTimeControl,
+  JsonFormsRendererRegistryEntry,
+  JsonSchema,
+  rankWith,
 } from '@jsonforms/core';
-import { defineComponent } from '../vue';
 import {
   rendererProps,
-  useJsonFormsControl,
   RendererProps,
+  useJsonFormsControl,
 } from '@jsonforms/vue2';
+import { ref } from '@vue/composition-api';
+import { VueMaskDirective as Mask } from 'v-mask';
+import {
+  VBtn,
+  VHover,
+  VIcon,
+  VMenu,
+  VSpacer,
+  VTextField,
+  VTimePicker,
+} from 'vuetify/lib';
+import { parseDateTime, useTranslator, useVuetifyControl } from '../util';
+import { defineComponent } from '../vue';
 import { default as ControlWrapper } from './ControlWrapper.vue';
-import { useVuetifyControl } from '../util';
-import { VTextField } from 'vuetify/lib';
+import { DisabledIconFocus } from './directives';
+
+const JSON_SCHEMA_TIME_FORMATS = [
+  'HH:mm:ss.SSSZ',
+  'HH:mm:ss.SSS',
+  'HH:mm:ssZ',
+  'HH:mm:ss',
+];
+
+// https://ajv.js.org/packages/ajv-formats.html#keywords-to-compare-values-formatmaximum-formatminimum-and-formatexclusivemaximum-formatexclusiveminimum
+type AjvMinMaxFormat = {
+  formatMinimum?: string | { $data: any };
+  formatExclusiveMinimum?: string | { $data: any };
+  formatMaximum?: string | { $data: any };
+  formatExclusiveMaximum?: string | { $data: any };
+};
 
 const controlRenderer = defineComponent({
   name: 'time-control-renderer',
   components: {
     ControlWrapper,
+    VHover,
     VTextField,
+    VMenu,
+    VTimePicker,
+    VIcon,
+    VSpacer,
+    VBtn,
   },
+  directives: { DisabledIconFocus, Mask },
   props: {
     ...rendererProps<ControlElement>(),
   },
   setup(props: RendererProps<ControlElement>) {
-    return useVuetifyControl(
-      useJsonFormsControl(props),
-      (value) => value || undefined,
-      300
+    const t = useTranslator();
+
+    const showMenu = ref(false);
+    const mask = ref<((value: string) => (string | RegExp)[]) | undefined>(
+      undefined
     );
+
+    const control = useVuetifyControl(
+      useJsonFormsControl(props),
+      (value: any) => value || undefined
+    );
+
+    return { ...control, showMenu, mask, t };
+  },
+  watch: {
+    isFocused(newFocus) {
+      if (newFocus) {
+        this.mask = this.maskFunction;
+      } else {
+        this.mask = undefined;
+      }
+    },
+  },
+  computed: {
+    timeFormat(): string {
+      return typeof this.appliedOptions.timeFormat == 'string'
+        ? this.appliedOptions.timeFormat
+        : this.ampm
+        ? 'hh:mm a'
+        : 'HH:mm';
+    },
+    timeSaveFormat(): string {
+      return typeof this.appliedOptions.timeSaveFormat == 'string'
+        ? this.appliedOptions.timeSaveFormat
+        : 'HH:mm:ss';
+    },
+    formats(): string[] {
+      return [
+        this.timeSaveFormat,
+        this.timeFormat,
+        ...JSON_SCHEMA_TIME_FORMATS,
+      ];
+    },
+    useSeconds(): boolean {
+      return this.timeFormat.includes('s') ? true : false;
+    },
+    ampm(): boolean {
+      return this.appliedOptions.ampm === true;
+    },
+    min(): string | undefined {
+      // provide min so that the browser can display the native component with only selections that are allowed.
+      // Since the browser supports only min there is posibility for the user to select a date that is defined in the formatExclusiveMinimum but the ajv will catch that validation.
+      const schema = this.control.schema as JsonSchema & AjvMinMaxFormat;
+      if (typeof schema.formatMinimum === 'string') {
+        return schema.formatMinimum;
+      } else if (typeof schema.formatExclusiveMinimum === 'string') {
+        return schema.formatExclusiveMinimum;
+      }
+      return undefined;
+    },
+    max(): string | undefined {
+      // provide max so that the browser can display the native component with only selections that are allowed.
+      // Since the browser supports only max there is posibility for the user to select a date that is defined in the formatExclusiveMaximum but the ajv will catch that validation.
+      const schema = this.control.schema as JsonSchema & AjvMinMaxFormat;
+      if (typeof schema.formatMaximum === 'string') {
+        return schema.formatMaximum;
+      } else if (typeof schema.formatExclusiveMaximum === 'string') {
+        return schema.formatExclusiveMaximum;
+      }
+      return undefined;
+    },
+    inputValue(): string | undefined {
+      const value = this.control.data;
+      const time = parseDateTime(value, this.formats);
+      return time ? time.format(this.timeFormat) : value;
+    },
+    pickerValue(): string | undefined {
+      const value = this.control.data;
+
+      const time = parseDateTime(value, this.formats);
+      return time
+        ? this.useSeconds
+          ? time.format('HH:mm:ss')
+          : time.format('HH:mm')
+        : value;
+    },
+    clearLabel(): string {
+      const label =
+        typeof this.appliedOptions.clearLabel == 'string'
+          ? this.appliedOptions.clearLabel
+          : 'Clear';
+
+      return label;
+    },
+    cancelLabel(): string {
+      const label =
+        typeof this.appliedOptions.cancelLabel == 'string'
+          ? this.appliedOptions.cancelLabel
+          : 'Cancel';
+
+      return label;
+    },
+    okLabel(): string {
+      const label =
+        typeof this.appliedOptions.okLabel == 'string'
+          ? this.appliedOptions.okLabel
+          : 'OK';
+      return label;
+    },
+  },
+  methods: {
+    onInputChange(value: string): void {
+      const time = parseDateTime(value, this.timeFormat);
+      this.onChange(time ? time.format(this.timeSaveFormat) : value);
+    },
+    onPickerChange(value: string): void {
+      const time = parseDateTime(value, this.useSeconds ? 'HH:mm:ss' : 'HH:mm');
+      this.onChange(time ? time.format(this.timeSaveFormat) : value);
+    },
+    clear(): void {
+      this.mask = undefined;
+      this.onChange(null);
+    },
+    maskFunction(value: string): (string | RegExp)[] {
+      const format = this.timeFormat;
+      const parts = format.split(/([^HhmsAaSZ]*)(hh?|HH?|mm?|ss?|a|A|SSS|Z)/);
+
+      let index = -1;
+      const numbers = value?.replace(/[^0-9]/g, '');
+
+      let result: (string | RegExp)[] = [];
+      for (const part of parts) {
+        if (part && part !== '') {
+          if (part == 'H') {
+            result.push(/[0-9]/);
+            index += 1;
+            if (numbers.charAt(index) === '1') {
+              result.push(/[0-9]/);
+              index += 1;
+            } else if (numbers.charAt(index) === '2') {
+              result.push(/[0-3]/);
+              index += 1;
+            }
+          } else if (part == 'HH') {
+            result.push(/[0-2]/);
+            index += 1;
+            result.push(numbers.charAt(index) === '2' ? /[0-3]/ : /[0-9]/);
+            index += 1;
+          } else if (part == 'h') {
+            result.push(/[1]/);
+            result.push(/[0-2]/);
+            index += 2;
+          } else if (part == 'hh') {
+            result.push(/[0-1]/);
+            index += 1;
+            result.push(numbers.charAt(index) === '0' ? /[1-9]/ : /[0-2]/);
+            index += 1;
+          } else if (part == 'm') {
+            result.push(/[0-9]/);
+            index += 1;
+            if (
+              numbers.charAt(index) === '1' ||
+              numbers.charAt(index) === '2' ||
+              numbers.charAt(index) === '3' ||
+              numbers.charAt(index) === '4' ||
+              numbers.charAt(index) === '5'
+            ) {
+              result.push(/[0-9]/);
+              index += 1;
+            }
+          } else if (part == 'mm') {
+            result.push(/[0-5]/);
+            result.push(/[0-9]/);
+            index += 2;
+          } else if (part == 's') {
+            result.push(/[0-9]/);
+            index += 1;
+            if (
+              numbers.charAt(index) === '1' ||
+              numbers.charAt(index) === '2' ||
+              numbers.charAt(index) === '3' ||
+              numbers.charAt(index) === '4' ||
+              numbers.charAt(index) === '5'
+            ) {
+              result.push(/[0-9]/);
+              index += 1;
+            }
+          } else if (part == 'ss') {
+            result.push(/[0-5]/);
+            result.push(/[0-9]/);
+            index += 2;
+          } else if (part == 'a') {
+            result.push(/a|p/);
+            result.push('m');
+          } else if (part == 'A') {
+            result.push(/A|P/);
+            result.push('M');
+          } else if (part == 'Z') {
+            //GMT-12 to GMT+14
+            result.push(/\+|-/);
+            result.push(/[0-1]/);
+            index += 1;
+            if (value.includes('-0') || value.includes('+0')) {
+              result.push(/[0-9]/);
+              index += 1;
+            } else if (value.includes('-1') || value.includes('+1')) {
+              result.push(value.includes('+1') ? /[0-4]/ : /[0-2]/);
+              index += 1;
+            }
+            result.push(':');
+            result.push(/[0-5]/);
+            result.push(/[0-9]/);
+            index += 2;
+          } else if (part == 'SSS') {
+            result.push(/[0-9]/);
+            result.push(/[0-9]/);
+            result.push(/[0-9]/);
+            index += 3;
+          } else {
+            result.push(part);
+          }
+        }
+      }
+
+      return result;
+    },
   },
 });
 
