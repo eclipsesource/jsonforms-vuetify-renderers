128a21fe3d9d0f1f6ea4b8bf32de11bcfdb6a881
Krasimir Chobantonov <kchobantonov@users.noreply.github.com>
2022-10-21 08:58:37 -0400

    Update vue2-vuetify/src/complex/ArrayControlRenderer.vue
    Co-authored-by: Lucas Koehler <lkoehler@eclipsesource.com>

--- vue2-vuetify/src/complex/ArrayControlRenderer.vue
+++ vue2-vuetify/src/complex/ArrayControlRenderer.vue
@@ -161,7 +161,7 @@
           </v-simple-table>
         </v-row>
       </v-container>
-      <v-container v-if="dataLength == 0" :class="styles.arrayList.noData">
+      <v-container v-if="dataLength === 0" :class="styles.arrayList.noData">
         No data
       </v-container>
     </v-card-text>
