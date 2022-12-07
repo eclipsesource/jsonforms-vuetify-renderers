b2aaa72b77259966bc17b8bffc0f6b98d9fd0966
kchobantonov <kchobantonov@gmail.com>
2022-12-07 17:09:32 -0500

    add clear when hover, do not use the explicitly type="number" because it works strange under chome when using values such as e121 or values larger than Number.MAX_VALUE the returned string from the component is empty string. If we can't convert to number then pass the original string so that AJV will report an error

--- vue2-vuetify/src/controls/IntegerControlRenderer.vue
+++ vue2-vuetify/src/controls/IntegerControlRenderer.vue
@@ -5,25 +5,27 @@
     :isFocused="isFocused"
     :appliedOptions="appliedOptions"
   >
-    <v-text-field
-      type="number"
-      :step="step"
-      :id="control.id + '-input'"
-      :class="styles.control.input"
-      :disabled="!control.enabled"
-      :autofocus="appliedOptions.focus"
-      :placeholder="appliedOptions.placeholder"
-      :label="computedLabel"
-      :hint="control.description"
-      :persistent-hint="persistentHint()"
-      :required="control.required"
-      :error-messages="control.errors"
-      :value="control.data"
-      v-bind="vuetifyProps('v-text-field')"
-      @input="onChange"
-      @focus="isFocused = true"
-      @blur="isFocused = false"
-    />
+    <v-hover v-slot="{ hover }">
+      <v-text-field
+        :step="step"
+        :id="control.id + '-input'"
+        :class="styles.control.input"
+        :disabled="!control.enabled"
+        :autofocus="appliedOptions.focus"
+        :placeholder="appliedOptions.placeholder"
+        :label="computedLabel"
+        :hint="control.description"
+        :persistent-hint="persistentHint()"
+        :required="control.required"
+        :error-messages="control.errors"
+        :value="control.data"
+        :clearable="hover"
+        v-bind="vuetifyProps('v-text-field')"
+        @input="onInputChange"
+        @focus="isFocused = true"
+        @blur="isFocused = false"
+      ></v-text-field>
+    </v-hover>
   </control-wrapper>
 </template>
 
@@ -42,12 +44,13 @@ import {
 } from '@jsonforms/vue2';
 import { default as ControlWrapper } from './ControlWrapper.vue';
 import { useVuetifyControl } from '../util';
-import { VTextField } from 'vuetify/lib';
+import { VHover, VTextField } from 'vuetify/lib';
 
 const controlRenderer = defineComponent({
   name: 'integer-control-renderer',
   components: {
     ControlWrapper,
+    VHover,
     VTextField,
   },
   props: {
@@ -56,7 +59,7 @@ const controlRenderer = defineComponent({
   setup(props: RendererProps<ControlElement>) {
     return useVuetifyControl(
       useJsonFormsControl(props),
-      (value) => parseFloat(value) || undefined,
+      (value: any) => value || undefined,
       300
     );
   },
@@ -64,6 +67,21 @@ const controlRenderer = defineComponent({
     step(): number {
       const options: any = this.appliedOptions;
       return options.step ?? 1;
+    },
+  },
+  methods: {
+    onInputChange(value: string): void {
+      const result = parseFloat(value);
+      if (
+        // the number is too large, too small or NaN
+        !isFinite(result) ||
+        !isFinite(Number(value)) // make sure that if we have any letters inside the number this will result in invalid number - Number() parses the string with different rules than parseFloat
+      ) {
+        // if we can't convert successfully the value then preserve the value that is entered but AJV will mark that as invalid
+        this.onChange(value);
+      } else {
+        this.onChange(result);
+      }
     },
   },
 });
